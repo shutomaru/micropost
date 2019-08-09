9ece3104827dd1f97311f44410669bb512a8db18
shuto.maru <answerwanashi@gmail.com>
2019-08-09 00:44:01 +0000

    modify login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
             {!! Form::open(['route'=>'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email','Email') !!}
-                    {!! Form::email('email',old('email',['class'=>'form-control'])) !!}
+                    {!! Form::email('email',old('email'),['class'=>'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('password','Password') !!}
