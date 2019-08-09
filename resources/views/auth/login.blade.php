60337540a888220e2a3349276f8060b215817c22
shuto.maru <answerwanashi@gmail.com>
2019-08-09 00:38:50 +0000

    modify login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     </div>
     
     <div class="row">
-        <div class="col-sm-6" offset-sm-3>
+        <div class="col-sm-6 offset-sm-3">
             {!! Form::open(['route'=>'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email','Email') !!}
@@ -14,7 +14,7 @@
                 </div>
                 <div class="form-group">
                     {!! Form::label('password','Password') !!}
-                    {!! Form::password('password',['class'=>'form-control'])) !!}
+                    {!! Form::password('password',['class'=>'form-control']) !!}
                 </div>
                 {!! Form::submit('Log in',['class'=>'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
