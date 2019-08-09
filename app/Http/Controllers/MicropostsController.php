f95e365d191efd2ed65c5f5b3ae0c5b91d0fad0a
shuto.maru <answerwanashi@gmail.com>
2019-08-09 02:41:23 +0000

    modify MicropostsController.php

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -10,7 +10,7 @@ class MicropostsController extends Controller
         $data=[];
         if(\Auth::check()){
             $user=\Auth::user();
-            $microposts=$user->microposts()->orderBy('created_at','desc')->paginate(10);
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
             
             $data=[
                 'user'=>$user,
