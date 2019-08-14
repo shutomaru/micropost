20c3659d7ee36d86a775c69a1a5657471b2d1d8d
shuto.maru <answerwanashi@gmail.com>
2019-08-14 07:57:41 +0000

    modify something

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -2,23 +2,12 @@
 
 @section('content')
     <div class="row">
-        <aside>
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ $user->name }}</h3>
-                </div>
-                <div class="card-body">
-                    <img class="rounded img-fluid" src="{{ Gravatar::src($user->email,500) }}" alt="">
-                </div>
-            </div>
-            @include('user_follow.follow_button',['user'=>$user])
+        <aside class="col-sm-4">
+           @include('users.card',['user'=>$user])
         </aside>
         <div class="col-sm-8">
-            <ul>
-                <li class="nav-item"><a href="{{ route('users.show',['id'=>$user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active': '' }}">TimeLine <span class="badge badge-secondary">{{ $count_microposts }}</span></a></li>
-                <li class="nav-item"><a href="{{ route('users.followings',['id'=>$user->id]) }}" class="nav-link {{ Request::is('users/*/followings') ? 'active' : '' }}">Followings <span class="badge badge-secondary">{{ $count_followings }}</span></a></li>
-                <li class="nav-item"><a href="{{ route('users.followers',['id'=>$user->id]) }}" class="nav-link {{ Request::is('users/*/followers') ? 'active' : '' }}">Followers <span class="badge badge-secondary">{{ $count_followers }}</span></a></li>
-            @include('users.users',['users'=>$users]
+            @include('users.navtabs',['user'=>$user])
+            @include('users.users',['users'=>$users])
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::group(['middleware'=>['auth']],function(){
     Route::resource('users','UsersController',['only'=>['index','show']]);
     Route::resource('microposts','MicropostsController',['only'=>['store','destroy']]);
     Route::group(['prefix'=>'users/{id}'],function(){
-       Route::post('follow','UserFollowController@store')->name('user.follow');
-       Route::delete('unfollow','UserFollowController@destroy')->name('user.unfollow');
-       Route::get('followings','UserController@followings')->name('users.followings');
-       Route::get('followers','UserController@followers')->name('users.followers');
+       Route::post('follow','UsersFollowController@store')->name('user.follow');
+       Route::delete('unfollow','UsersFollowController@destroy')->name('user.unfollow');
+       Route::get('followings','UsersController@followings')->name('users.followings');
+       Route::get('followers','UsersController@followers')->name('users.followers');
     });
     
     Route::resource('microposts','MicropostsController',['only'=>['store','destroy']]);
