ab9feaf0624e74ffd85d80a9c083f56a5b0e5e43
shuto.maru <answerwanashi@gmail.com>
2019-08-14 01:34:41 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -68,6 +68,12 @@ class User extends Authenticatable
         return $this->followings()->where('follow_id',$userId)->exists();
     }
     
+    public function feed_microposts(){
+        $follow_user_ids = $this->folloings()->pluck('users.id')->toArray();
+        $follow_user_ids[]=$this->id;
+        return Micropost::whereIn('user_id',$follow_user_ids);
+    }
+    
     
     
 }
